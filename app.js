9f07a5155341f83a6e11a5408bedc6e252aa4466
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-25 20:27:49 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieparser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controller/errorController')
@@ -25,6 +26,15 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors())
+// Access-Control-Allow-Origin
+// app.use(cors({
+//   origin:'https://www.natours.com'
+// }))
+
+app.options('*', cors())
+
 // Serving static files
 // app.use(express.static(`${__dirname}/public`))
 app.use(express.static(path.join(__dirname, 'public')))
