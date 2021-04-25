f7d04badcf4344501c51a1e61b03dcca22808913
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-25 15:53:56 +0530

    First commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieparser = require('cookie-parser')
+const compression = require('compression')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controller/errorController')
@@ -82,6 +83,8 @@ app.use(
     ],
   })
 )
+
+app.use(compression())
 
 // Test middleware
 app.use((req, res, next) => {
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,16 +7,13 @@ export const bookTour = async (tourId) => {
       'pk_test_51IjpqxSCBrmVKbatVl2XPccKtPpv8f0hAPeY9YODoaSJt2ZgLeND3iJMxvvxmAw7nWVsy3N7FkdSnn9kQCWI6JIw00RfKgIBdu'
     )
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/booking/checkout/${tourId}`
-    )
+    const session = await axios(`/api/v1/booking/checkout/${tourId}`)
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     })
   } catch (err) {
-    console.log(err)
-    showAlert('error', err)
+    showAlert('error', 'Something went wrong. Try again after sometimes!')
   }
 }
