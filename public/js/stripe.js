6436e08588a3d921ad0fd4c6879192452e022954
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-26 20:38:40 +0530

    Changes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import { showAlert } from './alerts'
 
-const stripe = Stripe(
-  'pk_test_51IjpqxSCBrmVKbatVl2XPccKtPpv8f0hAPeY9YODoaSJt2ZgLeND3iJMxvvxmAw7nWVsy3N7FkdSnn9kQCWI6JIw00RfKgIBdu'
-)
 export const bookTour = async (tourId) => {
   try {
+    const stripe = Stripe(
+      'pk_test_51IjpqxSCBrmVKbatVl2XPccKtPpv8f0hAPeY9YODoaSJt2ZgLeND3iJMxvvxmAw7nWVsy3N7FkdSnn9kQCWI6JIw00RfKgIBdu'
+    )
     // 1) Get checkout session from API
     const session = await axios(
       `http://127.0.0.1:3000/api/v1/booking/checkout/${tourId}`
