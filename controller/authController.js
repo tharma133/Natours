009d39d54b298fc84249d7fdd92afa7b15464a01
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-26 21:16:41 +0530

    Some

--- controller/authController.js
+++ controller/authController.js
@@ -56,7 +56,6 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401))
   }
-
   // 3) If everything ok, send token to client
   createSendToken(user, 200, req, res)
 })
