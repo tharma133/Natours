0351ed33df1a8c8415e7a7f6359341fbd37eec59
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-27 09:16:38 +0530

    package

--- controller/authController.js
+++ controller/authController.js
@@ -19,7 +19,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
   })
   // Remove password from output
   user.password = undefined
