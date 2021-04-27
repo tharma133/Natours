f17bb661c1e8f237f9c7c294ed7cbb8ad9859f19
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-04-27 09:42:11 +0530

    Changes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ export const bookTour = async (tourId) => {
       'pk_test_51IjpqxSCBrmVKbatVl2XPccKtPpv8f0hAPeY9YODoaSJt2ZgLeND3iJMxvvxmAw7nWVsy3N7FkdSnn9kQCWI6JIw00RfKgIBdu'
     )
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/booking/checkout/${tourId}`
-    )
+    const session = await axios(`/api/v1/booking/checkout/${tourId}`)
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
